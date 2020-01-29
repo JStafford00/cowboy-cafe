b11a21b94a61308b62ee8e98b339d0dcd9f9496c
Jordan Stafford <jstafford00@ksu.edu>
2020-01-29 15:17:49 -0600

    update3

---
+++ Data/TexasTripleBurger.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public class TexasTripleBurger
+    {
+        private bool bun = true;
+        private bool bacon = true;
+        private bool egg = true;
+        private bool ketchup = true;
+        private bool lettuce = true;
+        private bool tomato = true;
+        private bool cheese = true;
+        private bool mayo = true;
+        private bool mustard = true;
+        private bool pickle = true;
+
+
+        /// <summary>
+        /// Price of a Texas Triple
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return 6.45;
+            }
+        }
+
+        /// <summary>
+        /// Calories in a Texas Triple
+        /// </summary>
+        public uint Calories
+        {
+            get
+            {
+                return 698;
+            }
+        }
+
+        /// <summary>
+        /// If bacon is on a Texas Triple
+        /// </summary>
+        public bool Bacon
+        {
+            get
+            {
+                return bacon;
+            }
+
+            set
+            {
+                bacon = value;
+            }
+        }
+
+        /// <summary>
+        /// If egg is on your Texas Triple
+        /// </summary>
+        public bool Egg
+        {
+            get
+            {
+                return egg;
+            }
+
+            set
+            {
+                egg = value;
+            }
+        }
+
+        /// <summary>
+        /// If a bun is on your Texas Triple
+        /// </summary>
+        public bool Bun
+        {
+            get
+            {
+                return bun;
+            }
+
+            set
+            {
+                bun = value;
+            }
+        }
+
+        /// <summary>
+        /// If cheese is on your Texas Triple
+        /// </summary>
+        public bool Cheese
+        {
+            get
+            {
+                return cheese;
+            }
+
+            set
+            {
+                cheese = value;
+            }
+        }
+
+        /// <summary>
+        /// If ketchup is on your Texas Triple
+        /// </summary>
+        public bool Ketchup
+        {
+            get
+            {
+                return ketchup;
+            }
+
+            set
+            {
+                ketchup = value;
+            }
+        }
+
+        /// <summary>
+        /// If lettuce is on your Texas Triple
+        /// </summary>
+        public bool Lettuce
+        {
+            get
+            {
+                return lettuce;
+            }
+
+            set
+            {
+                lettuce = value;
+            }
+        }
+
+        /// <summary>
+        /// If mayo is on your Texas Triple
+        /// </summary>
+        public bool Mayo
+        {
+            get
+            {
+                return mayo;
+            }
+
+            set
+            {
+                mayo = value;
+            }
+        }
+
+        /// <summary>
+        /// If mustard is on your Texas Triple
+        /// </summary>
+        public bool Mustard
+        {
+            get
+            {
+                return mustard;
+            }
+
+            set
+            {
+                mustard = value;
+            }
+        }
+
+        /// <summary>
+        /// If tomato is on your Texas Triple
+        /// </summary>
+        public bool Tomato
+        {
+            get
+            {
+                return tomato;
+            }
+
+            set
+            {
+                tomato = value;
+            }
+        }
+
+        /// <summary>
+        /// If you want pickles on your Texas Triple
+        /// </summary>
+        public bool Pickle
+        {
+            get
+            {
+                return pickle;
+            }
+
+            set
+            {
+                pickle = value;
+            }
+        }
+
+
+
+        /// <summary>
+        /// Special Instructions 
+        /// </summary>
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                var instructions = new List<string>();
+
+                if (!bacon)
+                    instructions.Add("hold bacon");
+                if (!egg)
+                    instructions.Add("hold egg");
+                if (!bun)
+                    instructions.Add("hold bun");
+                if (!cheese)
+                    instructions.Add("hold cheese");
+                if (!lettuce)
+                    instructions.Add("hold lettuce");
+                if (!mustard)
+                    instructions.Add("hold mustard");
+                if (!pickle)
+                    instructions.Add("hold pickle");
+                if (!ketchup)
+                    instructions.Add("hold ketchup");
+                if (!tomato)
+                    instructions.Add("hold tomato");
+                if (!mayo)
+                    instructions.Add("hold mayo");
+
+                return instructions;
+            }
+        }
+    }
+}
