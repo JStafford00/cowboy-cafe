e224a4382a85a1f60acc6bc23f08e73390bffdb1
Jordan Stafford <jstafford00@ksu.edu>
2020-02-03 14:48:13 -0600

    beta1

---
+++ Data/Entree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public abstract class Entree
+    {
+        /// <summary>
+        /// Price of a entree.
+        /// </summary>
+        public abstract double Price { get; set; }
+
+        /// <summary>
+        /// Calories in the entree.
+        /// </summary>
+        public abstract uint Calories { get; set; }
+
+        /// <summary>
+        /// Special Instructions for the entree.
+        /// </summary>
+        public abstract List<String> SpecialInstructions { get; set; }
+    }
+}
