8d3c4d54b8521af39eb55857594104aeb69c4cb4
Jordan Stafford <jstafford00@ksu.edu>
2020-02-05 13:37:21 -0600

    Finished Corn Dodgers

---
+++ Data/CornDodgers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public class CornDodgers : Side
+    {
+        /// <summary>
+        /// Price of Corn Dodgers
+        /// </summary>
+        public override double Price
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Small:
+                        return 1.59;
+                    case Size.Medium:
+                        return 1.79;
+                    case Size.Large:
+                        return 1.99;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Calroies in Corn Dodgers
+        /// </summary>
+        public override uint Calories
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Small:
+                        return 512;
+                    case Size.Medium:
+                        return 685;
+                    case Size.Large:
+                        return 717;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
+    }
+}
